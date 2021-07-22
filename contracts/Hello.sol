413f38c8b2a0f2810bd2dd51b6514be8d491e459
beedu18 <b1n4y4k@gmail.com>
2021-07-22 11:36:58 +0530

    checks in functions - paying ether and validating accounts

--- contracts/Hello.sol
+++ contracts/Hello.sol
@@ -2,7 +2,26 @@
 pragma solidity 0.8.6;
 
 contract Hello {
-    function hello() public pure returns(string memory) {
-        return "Hello World";
+
+    string public message;
+    address public owner;
+
+    constructor(string memory _message) {
+        message = _message;
+        owner = msg.sender;
+    }
+
+    function hello() public view returns(string memory) {
+        return message;
+    }
+
+    function setMessage(string memory _message) public payable {
+        //first check if modifier is owner
+        require(msg.sender == owner, "Can only be modified my deployer");
+        
+        //then check if price requirement is fulfilled
+        require(msg.value > 1 ether, "Not Enough Ether");
+        
+        message = _message;
     }
 }
--- migrations/2_hello_migration.js
+++ migrations/2_hello_migration.js
@@ -1,5 +1,5 @@
 const HelloWorld = artifacts.require("Hello"); //require the contract name not the filename
 
 module.exports = function (deployer) {
-  deployer.deploy(HelloWorld);
+  deployer.deploy(HelloWorld, "constructor arguments will come here");
 };
